9169b6a065a6ab342368a6c45ad433025fce3b68
Doosies <song961003@gmail.com>
2021-10-21 15:58:41 +0900

    Switch

--- front/src/Router/App.tsx
+++ front/src/Router/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router';
+import { Route, RouteComponentProps, Switch } from 'react-router';
 import { Link } from 'react-router-dom';
 import About from './About';
 import HistorySample from './HistorySample';
@@ -18,10 +18,22 @@ function App() {
                     <Link to="/about">소개</Link>
                 </li>
             </ul>
-            <Route path="/" component={Home} exact/>
-            <Route path="/about" component={About} />
-            <Route path="/profiles" component={Profiles} />
-            <Route path="/history" component={HistorySample}/>
+            <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/about" component={About} />
+                <Route path="/profiles" component={Profiles} />
+                <Route path="/history" component={HistorySample}/>
+                <Route
+                    //path를 정의하지 않으면 모든상황
+                    render={({location}:RouteComponentProps)=>(
+                        <div>
+                            <h2>이 페이지는 존재하지 않습니다.</h2>
+                            <p>{location.pathname}</p>
+                        </div>
+                    )}
+                />
+            </Switch>
+            
         </div>
     )
 }
